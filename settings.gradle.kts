3c0e928d6deced208052d30b814a45506068c8d4
Han jun Kwon <hanjun.kwon.hs@gmail.com>
2023-11-16 13:54:17 +0900

    Project 초기 설정

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "EmptyDialogSample"
+include(":app")
+include(":EmptyDialog")
